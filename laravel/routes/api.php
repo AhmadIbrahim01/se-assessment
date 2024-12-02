24bb80df4ae516dbb5f622a3eef2bf7b884f861f
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-02 12:30:45 +0200

    Laravel <> Add: Api Route

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\UserController;
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post("/add",[UserController::class, "add_user"]);
+
