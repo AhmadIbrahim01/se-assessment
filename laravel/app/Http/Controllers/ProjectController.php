26ee0b048995a0bce565ecbe92bbb58617d06401
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-12-02 12:26:49 +0200

    Laravel <> Add: Edit And Delete Project Methods

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -32,4 +32,26 @@ class ProjectController extends Controller
         return response()->json($project);
 }
 
+    public function edit_project(Request $request, $id){
+        $project = Project::find($id);
+        if (!$project) {
+            return response()->json(['message' => 'project not found'], 404);
+        }
+
+        $project->update([
+            'name' => $request->$name,
+            'description' => $request->$description,
+        ]);
+
+        return response()->json(['message' => 'Project edited successfully', 'user' => $user]);
+    }
+
+    public function delete_project(Request $request, $id){
+        $project = Project::find($id)->delete();
+
+        return response()->json([
+            "Deleted_project" => $project,
+        ]);
+    }
+
 }
